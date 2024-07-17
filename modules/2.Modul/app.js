fda0cb8cd13a11a7fc344a3bd745b80b44b327d2
Gynna Paola Mogollon Zabala <gynnapaolamzz@gmail.com>
2024-07-16 19:30:31 -0500

    actualizacion

--- modules/2.Modul/app.js
+++ modules/2.Modul/app.js
@@ -1,17 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
-function rqListener(req, res) {
-
-}
 //pasamos una funcion para crear un servidor y esto basicamente es un detector de eventos continuos 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Mesaage</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Hello Gy');//dentro de este archivo de escritura toma una ruta al archivo y solo se usara el nombre del archivo para crearlo en la misma carpeta de la app.js
+        res.statusCode = 302;
+        res.setHead('Location', '/');//permite escribir cierta metainformacion
+        return res.end();//esto es necesario para no tener error 
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
